98272c1198a2e92b1f473f0ba38bda70069b3fa9
Bakhoumalbert <bakhoum.albert@uam.edu.sn>
2025-08-01 18:33:48 +0000

    update after first deploiement

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,41 +4,53 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-# Utilisation du VPC par défaut
+# VPC par défaut
 data "aws_vpc" "default" {
   default = true
 }
 
-# Récupération d’un subnet par défaut
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+# Liste des subnets par défaut dans le VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
 }
 
-# Dernière AMI Ubuntu 22.04 LTS
+# AMI Ubuntu 22.04 LTS
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   filter {
     name   = "root-device-type"
     values = ["ebs"]
   }
 }
 
-# Groupe de sécurité : SSH + Prometheus + Grafana
+# Groupe de sécurité pour SSH, Grafana, Prometheus
 resource "aws_security_group" "monitoring_sg" {
   name        = "monitoring-sg"
   description = "Allow SSH, Grafana, and Prometheus"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -46,6 +58,7 @@ resource "aws_security_group" "monitoring_sg" {
   }
 
   ingress {
+    description = "Prometheus"
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
@@ -53,6 +66,7 @@ resource "aws_security_group" "monitoring_sg" {
   }
 
   ingress {
+    description = "Grafana"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
@@ -65,23 +79,26 @@ resource "aws_security_group" "monitoring_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "monitoring-security-group"
+  }
 }
 
-# Instance EC2 unique
+# Instance EC2 pour le monitoring
 resource "aws_instance" "monitoring_server" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   key_name                    = var.key_name
-  subnet_id                   = data.aws_subnet_ids.default.ids[0]
+  subnet_id                   = data.aws_subnets.default.ids[0] # ✅ sélection du premier subnet
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.monitoring_sg.id]
 
   tags = {
     Name = "monitoring-server"
   }
-}
+  lifecycle {
+    create_before_destroy = true
+  }
 
-# Sortie IP publique pour Ansible
-output "instance_ip" {
-  value = aws_instance.monitoring_server.public_ip
-}
+} 
